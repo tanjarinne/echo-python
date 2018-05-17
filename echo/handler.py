6802b592b6a5ca01cd7c39c8d5f2a84e0505e375
Mario Álvarez <ahoy@m4grio.me>
2018-05-18 04:03:14 +0800

    Add initial version

---
+++ echo/handler.py
@@ -0,0 +1,38 @@
+import socket
+
+from http.server import BaseHTTPRequestHandler, HTTPStatus
+
+
+class EchoHandler(BaseHTTPRequestHandler):
+    """
+    EchoHandler
+    """
+
+    def handle_one_request(self):
+        """Handle a single HTTP request.
+
+        You normally don't need to override this method; see the class
+        __doc__ string for information on how to handle specific HTTP
+        commands such as GET and POST.
+
+        """
+        try:
+            self.raw_requestline = self.rfile.readline(65537)
+            if len(self.raw_requestline) > 65536:
+                self.requestline = ''
+                self.request_version = ''
+                self.command = ''
+                self.send_error(HTTPStatus.REQUEST_URI_TOO_LONG)
+                return
+            if not self.raw_requestline:
+                self.close_connection = True
+                return
+            if not self.parse_request():
+                # An error code has been sent, just exit
+                return
+            self.do_any()
+            self.wfile.flush()
+        except socket.timeout as e:
+            self.log_error("Request timed out: %r", e)
+            self.close_connection = True
+            return
